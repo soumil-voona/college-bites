48b838db8cea80c6cfce161d9490e1a416457908
Iniyann <ini.vivek@gmail.com>
2025-03-08 14:55:35 -0600

    Login Screen JS fix

--- src/LoginScreen.js
+++ src/LoginScreen.js
@@ -18,29 +18,30 @@ function LoginScreen() {
   };
 
   return (
-    <div className="login-container">
-      <h2>Login Page</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={handleUsernameChange}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={handlePasswordChange}
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
+    <div>
+        <form style={{ backgroundColor: "#F6F6F6" , borderRadius: "10px" }}>
+            <div>
+                <input name="email" type='email' placeholder='Enter Email Address' />
+            </div>
+
+            <div>
+                <input name="password" type='password' placeholder='Enter Password'/>
+            </div>
+
+            <div>
+                <button type="submit">Login</button>
+            </div>
+
+            <div>
+                <button type='button'>Forgot Password?</button>
+            </div>
+
+            <div className='line'></div>
+
+            <div>
+                <button type='button'>Create New Account</button>
+            </div>
+        </form>
     </div>
   );
 }
