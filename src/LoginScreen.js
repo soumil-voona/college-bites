fcf5f759db64a92e0e4fb48b5530ec8d3c67e797
Iniyann <ini.vivek@gmail.com>
2025-03-08 14:22:58 -0600

    Created LoginScreen.js

---
+++ src/LoginScreen.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+
+function LoginScreen() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Username:', username, 'Password:', password);
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Login Page</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={handleUsernameChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginScreen;
