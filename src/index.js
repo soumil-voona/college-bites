6037ae851a289202f7278e96b95bcb3eeea2286d
sxv110 <sxv2480@gmail.com>
2025-03-08 20:00:02 -0600

    change routing

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import App from "./App";
 import LoginScreen from "./LoginScreen";
 import SignupScreen from "./SignupScreen";
 import Information from "./Information"
+import MainWebpage from "./MainWebpage"
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<App />} /> {/* Default route */}
+        <Route path="/" element={<MainWebpage />} /> {/* Default route */}
         <Route path="/login" element={<LoginScreen />} /> {/* Login route */}
         <Route path="/signup" element={<SignupScreen />} />
         <Route path="/info" element = {<Information />} />
