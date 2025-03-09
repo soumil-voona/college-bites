3fc8f2a23c2c142bfd9ecfd370d624604a68c96b
sxv110 <sxv2480@gmail.com>
2025-03-09 04:08:19 -0500

    a

--- src/index.js
+++ src/index.js
@@ -6,10 +6,7 @@ import SignupScreen from "./SignupScreen";
 import Information from "./Information"
 import MainWebpage from "./MainWebpage"
 import DriverPortal from "./driverPortal";
-<<<<<<< HEAD
-=======
 import Map from "./Map"
->>>>>>> be708728af84ff79ee5bfe71df7f13cd8904d749
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
@@ -18,14 +15,9 @@ root.render(
         <Route path="/" element={<Information />} /> {/* Default route */}
         <Route path="/login" element={<LoginScreen />} /> {/* Login route */}
         <Route path="/signup" element={<SignupScreen />} />
-<<<<<<< HEAD
-        <Route path="/info" element = {<Information />} />
-        <Route path="/driverPortal" element = {<DriverPortal/>} />
-=======
         <Route path="/select" element = {<MainWebpage />} />
         <Route path="/driverPortal" element = {<DriverPortal/>} />
         <Route path="/find" element = {<Map />} />
->>>>>>> be708728af84ff79ee5bfe71df7f13cd8904d749
       </Routes>
     </Router>
   </React.StrictMode>
