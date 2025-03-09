7514a5464da0935370a7eb7ce7f378384605d24f
sxv110 <sxv2480@gmail.com>
2025-03-09 03:18:29 -0500

    commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginScreen from "./LoginScreen";
-import SignupScreen from "./SignupScreen";
+import SignupScreen from "./SignupScreen-1";
 import Information from "./Information"
 import MainWebpage from "./MainWebpage"
 import DriverPortal from "./driverPortal";
