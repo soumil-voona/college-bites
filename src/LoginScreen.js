e8f346e605c9d2bec063b6b307eab5f3506e0cdd
sxv110 <sxv2480@gmail.com>
2025-03-15 13:19:06 -0500

    login - with popup

--- src/LoginScreen.js
+++ src/LoginScreen.js
@@ -1,7 +1,9 @@
 import { useState } from 'react';
 import { getAuth, GoogleAuthProvider, signInWithPopup, signInWithEmailAndPassword } from 'firebase/auth';
+
 import { useNavigate } from 'react-router-dom';
 import './login.css'; // Ensure this import is included to load the CSS
+import { getFirestore } from 'firebase/firestore';
 
 function App() {
   const auth = getAuth();
@@ -21,6 +23,10 @@ function App() {
       const response = await signInWithPopup(auth, new GoogleAuthProvider());
       console.log(response.user.uid);
       navigate('/');
+      const user = response.user;
+      const displayName = user.displayName;
+      const userEmail = user.email;
+      alert(`Welcome, ${displayName} (${userEmail})! \n You are now logged in`);
     } catch (error) {
       console.error('Error during sign-in with Google:', error);
       setAuthing(false);
@@ -37,6 +43,13 @@ function App() {
       const response = await signInWithEmailAndPassword(auth, email, password);
       console.log(response.user.uid);
       navigate('/');
+      const user = response.user;
+      // Fetch the user's display name from Firestore
+      const userDoc = await getFirestore().collection('users').doc(user.uid).get();
+      const displayName = userDoc.data().displayName;
+      const userEmail = user.email;
+      alert(`Welcome, ${displayName} (${userEmail})! \n You are now logged in`);
+      setAuthing(false);
     } catch (error) {
       console.error('Error during sign-in with email and password:', error);
       setError(error.message);
